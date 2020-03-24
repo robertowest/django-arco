df0176e5c68a2133157546df0d730cf54b4e8483
robertowest <roberto.west@gmail.com>
2020-03-24 10:35:47 -0300

    modificado en casa

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git add .
 # git reset push.sh
 git commit -m "modificado en casa"
 git push -u origin master
-git push heroku master
+# git push heroku master
 
 # heroku run python manage.py makemigrations
 # heroku run python manage.py migrate
